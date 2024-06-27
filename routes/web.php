07f140e35d4a0dc7adfce1af9d9d749fc2911e09
Abdrahman Moussa <maloom1414@gmail.com>
2024-06-27 23:22:21 +0300

    Create Routes

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,30 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// New Routes
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\CaseController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\SessionController;
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('tasks', TaskController::class);
+    Route::resource('invoices', InvoiceController::class);
+    Route::resource('cases', CaseController::class);
+    Route::resource('clients', ClientController::class);
+    Route::resource('roles', RoleController::class);
+    Route::resource('permissions', PermissionController::class);
+    Route::resource('sessions', SessionController::class);
 });
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
